43421249671cf3f2574cc69af3b75529430a2db0
Shuhratzhan.Nizamov <Shukhratzhon.Nizamov@nat.kz>
2019-06-18 17:29:24 +0600

    Initialize Party Invites project created on ASP.NET MVC 3

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour > 12 ? "Good Afternoon!" : "Good Morning!";
+            return View();
+        }
+
+    }
+}
