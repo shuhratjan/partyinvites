bba06ff62180d7cbf76d62b14c10f0f0bd6ac74a
Shuhratzhan.Nizamov <Shukhratzhon.Nizamov@nat.kz>
2019-06-19 10:22:39 +0600

    Creating RSVP Form

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -6,16 +6,29 @@ using System.Web.Mvc;
 
 namespace PartyInvites.Controllers
 {
+    using PartyInvites.Models;
+    using System.Diagnostics.CodeAnalysis;
+
     public class HomeController : Controller
     {
-        //
         // GET: /Home/
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour > 12 ? "Good Afternoon!" : "Good Morning!";
+            ViewBag.Greeting = hour > 12 ? "Good Afternoon" : "Good Morning";
             return View();
         }
 
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
     }
 }
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    { 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the will attend.
+        /// </summary>
+        public bool? WillAttend { get; set; }
+    }
+}
